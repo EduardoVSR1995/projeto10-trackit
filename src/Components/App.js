c4de73a489be56e952b50d97e3e1779e56f5fc4f
EduardoVSR1995 <eduardvitor7@gmail.com>
2022-08-02 13:09:24 -0300

    Arquivos react

---
+++ src/Components/App.js
@@ -0,0 +1,13 @@
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
+import Enter from './Enter';
+
+
+export default function App(){
+    <BrowserRouter>
+        <Routes>
+            <Route path='/' element={<Enter/>}/>
+            
+        </Routes>
+    
+    </BrowserRouter>
+}
---
+++ src/Components/Enter.js
@@ -0,0 +1,19 @@
+import styled from "styled-components"
+import logo from './image/logo.png'
+
+export default function Enter(){
+    return(
+
+        <Container>
+            <img src={logo}/>
+            
+             ollaaaaaaaaaa bbbbbbbbbbbbbb    bbbbbbbbbbb   aa</Container> 
+    )
+
+}
+
+const Container = styled.div`
+background-color: red;
+width: 100px;
+height: 100px;
+`;
